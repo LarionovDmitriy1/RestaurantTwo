cfd873683566eb47865a4dd0cd41700733908143
Студент1 <1@DESKTOP-7RO4MU5>
2022-12-02 09:20:10 +0300

    Добавьте файлы проекта.

---
+++ Restaurant1/MenuParser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant1
+{
+    public static class MenuParser
+    {
+
+        public static void Parser(string selected, List<string> menu)
+        {
+            List<string> strings = new List<string>();
+            List<string> menu2 = new List<string>();
+
+            foreach (var item in menu)
+            {
+                string item2 = item.ToLower();
+                menu2.Add(item2);
+            }
+            foreach (var item in menu2)
+            {
+
+                if (selected.Contains(item))
+                {
+                    strings.Add(item + ' ');
+                }
+            }
+            for (int i = 0; i < strings.Count; i++)
+            {
+                string s = strings[i];
+                s = Char.ToUpper(s[0]) + s.Remove(0, 1);
+                strings[i] = s;
+                Console.Write(strings[i]);
+            }
+
+        }
+    }
+}
---
+++ Restaurant1/Program.cs
@@ -0,0 +1,33 @@
+﻿using Restaurant1;
+
+void Menu()
+{
+    List<string> list = new List<string>()
+        {
+            "Burger","Fries","Chicken","Pizza","Sandwich","Onionrings","Milkshake","Coke"
+        };
+    Console.WriteLine();
+    Console.WriteLine("Меню ресторана");
+    Console.WriteLine();
+    foreach (var item in list)
+    {
+        Console.WriteLine(item);
+    }
+    Console.WriteLine();
+    Console.WriteLine("Ваш заказ");
+}
+void SelectedMenuItem()
+{
+    List<string> list = new List<string>()
+    {
+        "Burger","Fries","Chicken","Pizza","Sandwich","Onionrings","Milkshake","Coke"
+    };
+    string menu = Console.ReadLine();
+    MenuParser.Parser(menu, list);
+
+}
+while (true)
+{
+    Menu();
+    SelectedMenuItem();
+}
